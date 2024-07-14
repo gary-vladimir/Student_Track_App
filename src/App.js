fb5b476ca8005b1d7fa4965930dd3909ef7aac54
Gary Vladimir <gary@garybricks.com>
2024-07-14 15:20:20 -0600

    initial front end with fetching api data demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api/data')
+      .then(response => {
+        setData(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{data ? data : 'Loading...'}</h1>
       </header>
     </div>
   );
