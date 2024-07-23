3fd0b3bd62e7b1550ab21aa33692ffd5fb792e09
Gary Vladimir <gary@garybricks.com>
2024-07-22 20:06:28 -0600

    added Students page, displays all students and a form for adding a new one

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import CreateGroup from "./components/CreateGroup";
 import GroupDetails from "./components/GroupDetails";
+import Students from "./components/Students";
+
 const App = () => {
   return (
     <div className="bg-[#E7F9FA] p-36 min-h-screen">
@@ -12,6 +14,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/create-group" element={<CreateGroup />} />
           <Route path="/group/:id" element={<GroupDetails />} />
+          <Route path="/students" element={<Students />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Students.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import BackBtn from "../assets/BackButton.svg";
+
+const Students = () => {
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState("");
+  const [parentPhoneNumber, setParentPhoneNumber] = useState("");
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/api/students")
+      .then((response) => {
+        setStudents(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the students!", error);
+      });
+  }, []);
+
+  const handleAddStudent = (e) => {
+    e.preventDefault();
+    const newStudent = { name, parent_phone_number: parentPhoneNumber };
+
+    axios
+      .post("http://127.0.0.1:5000/api/students", newStudent)
+      .then((response) => {
+        setStudents([...students, response.data]);
+        setName("");
+        setParentPhoneNumber("");
+      })
+      .catch((error) => {
+        console.error("There was an error adding the student!", error);
+      });
+  };
+
+  if (loading) {
+    return <div className="text-center text-xl">Loading...</div>;
+  }
+
+  return (
+    <div className="flex">
+      <div className="w-1/2 p-6">
+        <button onClick={() => navigate("/")}>
+          <img
+            src={BackBtn}
+            alt="Back Button"
+            className="h-10 hover:scale-110 transition"
+          />
+        </button>
+        <h1 className="text-5xl font-bold mb-6 text-[#2F4858]">Students</h1>
+        <ul>
+          {students.map((student) => (
+            <li
+              key={student.id}
+              className="mb-4 p-4 border rounded bg-white shadow-md"
+            >
+              {student.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-1/2 p-6">
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Add New Student</h2>
+          <form onSubmit={handleAddStudent}>
+            <div className="mb-4">
+              <label className="block text-gray-700">Full Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-[#F26419]"
+                placeholder="Enter full name here"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Tutor Phone Number:</label>
+              <input
+                type="text"
+                id="parentPhoneNumber"
+                value={parentPhoneNumber}
+                onChange={(e) => setParentPhoneNumber(e.target.value)}
+                className="border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-[#F26419]"
+                placeholder="ex: +52 953 340 4382"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#69A1CB] text-white font-bold py-2 px-4 rounded hover:bg-[#4A90E2] transition"
+            >
+              CREATE
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Students;
