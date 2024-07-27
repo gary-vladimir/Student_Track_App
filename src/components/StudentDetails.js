e1311570401cf7a34aab49b2c2a00e7bea9ba888
Gary Vladimir <gary@garybricks.com>
2024-07-27 12:28:13 -0600

    made yes/no buttons larger for delete student confirmation

--- src/components/StudentDetails.js
+++ src/components/StudentDetails.js
@@ -392,18 +392,20 @@ const StudentDetails = () => {
             <p className="mb-4">
               Are you sure you want to delete this student?
             </p>
-            <button
-              onClick={handleDelete}
-              className="bg-red-500 text-white py-2 px-4 rounded mr-4"
-            >
-              Yes
-            </button>
-            <button
-              onClick={() => setShowConfirmPopup(false)}
-              className="bg-gray-500 text-white py-2 px-4 rounded"
-            >
-              No
-            </button>
+            <div className="flex w-full">
+              <button
+                onClick={handleDelete}
+                className="bg-red-500 text-white w-1/2 py-2 px-4 rounded mr-4"
+              >
+                Yes
+              </button>
+              <button
+                onClick={() => setShowConfirmPopup(false)}
+                className="bg-gray-500 text-white w-1/2 py-2 px-4 rounded"
+              >
+                No
+              </button>
+            </div>
           </div>
         </div>
       )}
