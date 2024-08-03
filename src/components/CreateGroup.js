f5d5ec9eb911ecf01f11041467814c02c058b86b
Gary Vladimir <gary@garybricks.com>
2024-08-03 14:30:26 -0600

    added authorization to CreateGroup component

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -1,7 +1,7 @@
-// src/components/CreateGroup.js
 import React, { useState } from "react";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 import BackBtn from "../assets/BackButton.svg";
 import decoration from "../assets/add_group_decoration.svg";
 
@@ -9,34 +9,45 @@ const CreateGroup = () => {
   const [title, setTitle] = useState("");
   const [groupCost, setGroupCost] = useState(200);
   const navigate = useNavigate();
+  const { getAccessTokenSilently } = useAuth0();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newGroup = { title, group_cost: parseInt(groupCost, 10) };
 
-    axios
-      .post("http://127.0.0.1:5000/api/groups", newGroup)
-      .then((response) => {
-        console.log("Group created:", response.data);
-        navigate("/"); // Redirect to home page after creating the group
-      })
-      .catch((error) => {
-        console.error("There was an error creating the group!", error);
+    try {
+      const token = await getAccessTokenSilently({
+        audience: "https://studenttrackapi.com",
       });
+
+      const response = await axios.post(
+        "http://127.0.0.1:5000/api/groups",
+        newGroup,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      console.log("Group created:", response.data);
+      navigate("/"); // Redirect to home page after creating the group
+    } catch (error) {
+      console.error("There was an error creating the group!", error);
+    }
   };
 
   return (
     <div>
       <h1 className="text-5xl flex font-bold mb-6 text-[#2F4858]">
-        {" "}
         <button onClick={() => navigate("/")}>
           <img
             src={BackBtn}
             alt="Add Group Icon"
             className="h-[40px] hover:scale-110 transition mt-1 mr-4"
           />
-        </button>{" "}
-        <div>Add Group</div>{" "}
+        </button>
+        <div>Add Group</div>
       </h1>
       <form
         className="border-2 relative z-10 border-[#69A1CB] p-8 backdrop-blur-lg bg-white/50 rounded-lg w-[750px] shadow-sm"
@@ -55,7 +66,7 @@ const CreateGroup = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="ex. English Beginners 7 - 8 am "
-            className=" appearance-none border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="appearance-none border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             required
           />
         </div>
@@ -71,7 +82,7 @@ const CreateGroup = () => {
             id="groupCost"
             value={groupCost}
             onChange={(e) => setGroupCost(e.target.value)}
-            className=" appearance-none border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="appearance-none border-b-2 border-[#69A1CB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             required
           />
         </div>
