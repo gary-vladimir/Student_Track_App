62206a2ed7b48b9f47948247d2ee229d70e0e65d
Gary Vladimir <gary@garybricks.com>
2024-08-03 18:30:14 -0600

    added new Logo to navBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Logo from "../assets/StudentTrackAppLogo.png";
 
 const Navbar = () => {
   const { logout, user } = useAuth0();
 
   return (
-    <nav className="bg-[#2F4858] z-50 p-4">
+    <nav className="bg-[#2F4858] p-2 px-20 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Student Track App</div>
+        <div className="text-white font-bold text-xl">
+          <img width="100px" src={Logo}></img>
+        </div>
         <div className="flex items-center">
           {user && <span className="text-white mr-4">{user.name}</span>}
           <button
