ac666843d85d01f709a3adf889ccddaf3af9c836
Gary Vladimir <gary@garybricks.com>
2024-08-05 22:57:13 -0600

    added create tables feature in manage.py for deploy

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,17 @@
+# manage.py
 from flask.cli import FlaskGroup
 from app import app, db
 from models import Group, Student, Payment
 
 cli = FlaskGroup(app)
+
+
+@cli.command("create_tables")
+def create_tables():
+    with app.app_context():
+        db.create_all()
+    print("Tables created successfully.")
+
 
 if __name__ == "__main__":
     cli()
